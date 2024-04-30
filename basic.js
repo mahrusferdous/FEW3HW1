006f9184fd6c58372d7c4f20763ca3adf3f65d3b
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-04-30 15:09:52 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,43 @@
+
+const name = "Tom Cat"
+let age = 27
+let grade = 95.5
+let attendanceStatus = true
+
+
+console.log("Student Name:", name);
+console.log("Student Age:", age);
+console.log("Student grade:", grade);
+console.log("Student attendance status:", attendanceStatus);
+
+
+// Exploring javascript operators 
+num1 = 10
+num2 = 37
+
+// Task 3: Perform arithmetic operations
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num1 / num2);
+
+
+// Task 4: Explore assignment operators
+num1 = 32
+num2 = -81
+
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+
+// Task 5: Use comparison operators
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?", num1 < num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+
+
+// Task 6: Apply logical operators
+let isPositive = num1 > 0 && num2 > 0; 
+let isEven = num1 % 2 == 0 || num2 % 2 == 0;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
